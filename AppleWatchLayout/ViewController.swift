c6fa84cad80ddb5db9730a702af8b3d17dd545e6
Artem Goryaev <artem.goryaev@hootsuite.com>
2016-12-30 21:38:58 -0800

    Initial Commit

---
+++ AppleWatchLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleWatchLayout
+//
+//  Created by new user on 2016-12-30.
+//  Copyright © 2016 Artem Goryaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
